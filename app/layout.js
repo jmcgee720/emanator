350faf7cb27cb6cc9ac8654a64326e44e81c9154
jmcgee720 <jmcgee720@gmail.com>
2026-05-23 14:14:13 -0400

    Auroraly agent: edit app/layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import './mobile-chat.css'
 import { Toaster } from '@/components/ui/toaster'
 import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
