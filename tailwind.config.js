32bc0f4d57db1f949ef11a9f5670de8ed9f86d31
jmcgee720 <jmcgee720@gmail.com>
2026-07-09 10:59:05 -0400

    Auroraly agent: edit tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,6 +90,10 @@ module.exports = {
     				to: {
     					height: '0'
     				}
+    			},
+    			'slide-up': {
+    				'0%': { transform: 'translateY(100%)', opacity: '0' },
+    				'100%': { transform: 'translateY(0)', opacity: '1' }
     			}
     		},
     		animation: {
